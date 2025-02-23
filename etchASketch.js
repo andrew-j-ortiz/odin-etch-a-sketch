145f0fbdf36344bf46edd52bce8629149b3b83e2
Andrew J Ortiz <112707094+andrew-j-ortiz@users.noreply.github.com>
2025-02-23 11:07:57 -0500

    Grids get colored when mouse hovers over them

--- etchASketch.js
+++ etchASketch.js
@@ -5,6 +5,10 @@ const domGridContainer = document.getElementById("gridContainer");
 function generateGrids(intGrids) {
     for (let index = 0; index < intGrids; index++) {
         const grid = document.createElement("div")
+        grid.id = "grid_" + index
+        grid.onmouseover = (event)=>{
+            document.getElementById(event.target.id).classList.add("grid_darkened")
+        }
         domGridContainer.appendChild(grid)
     }
 }
